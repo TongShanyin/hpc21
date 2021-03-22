3b3c5a782c2d2acfb7eda5d1f3706b94de50e467
Shanyin Tong <st3255@nyu.edu>
2021-03-22 15:52:18 -0400

    omp prob2

--- hw3/omp_solved2.c
+++ hw3/omp_solved2.c
@@ -1,21 +1,22 @@
 /******************************************************************************
 * FILE: omp_bug2.c
 * DESCRIPTION:
-*   Another OpenMP program with a bug. 
-* AUTHOR: Blaise Barney 
-* LAST REVISED: 04/06/05 
+*   Another OpenMP program with a bug.
+* AUTHOR: Blaise Barney
+* LAST REVISED: 04/06/05
 ******************************************************************************/
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
 int nthreads, i, tid;
 float total;
 
 /*** Spawn parallel region ***/
-#pragma omp parallel 
+// need to specify tid as a private variable, need to use reduction since we do sum.
+#pragma omp parallel private(tid) reduction(+:total)
   {
   /* Obtain thread number */
   tid = omp_get_thread_num();
@@ -31,10 +32,13 @@ float total;
   /* do some work */
   total = 0.0;
   #pragma omp for schedule(dynamic,10)
-  for (i=0; i<1000000; i++) 
+  for (i=0; i<1000000; i++)
      total = total + i*1.0;
 
-  printf ("Thread %d is done! Total= %e\n",tid,total);
-
+  // printf ("Thread %d is done! Total= %e\n",tid,total);
+    printf ("Thread %d is done!\n",tid);
   } /*** End of parallel region ***/
+  printf ("Total= %e\n",total); // print total when parallel ends
+  // the total value is different from the analytic solution, but I try using one thread, still not the same as the analytic one,
+  // might from the round-off error.
 }
