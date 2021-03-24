d2dabe2eedd050c6816b0d5e2b095a078d39a681
Shanyin Tong <st3255@nyu.edu>
2021-03-24 12:55:24 -0400

    jacobi

--- hw3/jacobi2D-omp.cpp
+++ hw3/jacobi2D-omp.cpp
@@ -1,7 +1,11 @@
 // Jacobi method for Laplace equation in 2D
+// To use openmp: g++ -std=c++11 -fopenmp jacobi2D-omp.cpp -o jacobi2D-omp && ./jacobi2D-omp 1000 100
+// Not use openmp: g++ -std=c++11  jacobi2D-omp.cpp -o jacobi2D && ./jacobi2D 1000 100
 
-#include <stdio.h>
+#include <algorithm>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #ifdef _OPENMP
 #include <omp.h>
 #else
@@ -10,7 +14,6 @@
 
 // Jacobi iteration update, s for storing the internal states
 void Jacobi(long N, double h, double *u, double *f, double *s){
-  // double h = 1/(N+1);
   #ifdef _OPENMP
     #pragma omp parallel for
   #endif
@@ -20,6 +23,9 @@ void Jacobi(long N, double h, double *u, double *f, double *s){
     }
   }
   #ifdef _OPENMP
+    #pragma omp barrier
+  #endif
+  #ifdef _OPENMP
     #pragma omp parallel for
   #endif
   for (long i=1; i <= N; i++){
@@ -27,27 +33,18 @@ void Jacobi(long N, double h, double *u, double *f, double *s){
       u[i+j*(N+2)] = (h*h*f[i+j*(N+2)] + s[i+j*(N+2)]) / 4;
     }
   }
-  // #ifdef _OPENMP
-  //   #pragma omp parallel for
-  // #endif
-  // for (long i=1; i <= N; i++){
-  //   for (long j = 1; j <= N; j++){
-  //     u[i+j*(N+2)] = (h*h*f[i+j*(N+2)] + u[i-1+j*(N+2)] + u[i+(j-1)*(N+2)] +
-  //      u[i+1+j*(N+2)] + u[i+(j+1)*(N+2)]) / 4;
-  //   }
-  // }
 }
 
 // compute residual
 double Residual(long N, double *u, double *f){
-  double res = 0;
+  double res = 0.;
   #ifdef _OPENMP
     #pragma omp parallel for reduction(+:res)
   #endif
   for (long i=1; i <= N; i++){
     for (long j = 1; j <= N; j++){
       res += pow((4*u[i+j*(N+2)] - u[i-1+j*(N+2)] - u[i+(j-1)*(N+2)] -
-      u[i+1+j*(N+2)] - u[i+(j+1)*(N+2)])*(N+1)*(N+1) - f[i+j*(N+2)], 2);
+       u[i+1+j*(N+2)] - u[i+(j+1)*(N+2)])*(N+1)*(N+1) - f[i+j*(N+2)], 2);
     }
   }
   res = pow(res, 0.5);
@@ -56,25 +53,31 @@ double Residual(long N, double *u, double *f){
 
 // Jacobi full iterations
 void Jacobi_iter(long N, double *u, double *f, double *s, int max_ite, double tol){
-  double h = 1/(N+1);
+  double h = 1./(N+1);
   double res0 = Residual(N, u, f);
   double rel = 1;
+  double res;
   int ite = 0;
-  printf("Jacobi Iterations:\n");
-  printf(" Iteration       Residual \n");
-  printf("%10d %10f \n", ite, res0);
+  // // uncomment this part if want to print iterations
+  // printf("Jacobi Iterations:\n");
+  // printf(" Iteration       Residual \n");
+  // printf("%10d %10f \n", ite, res0);
   while (ite < max_ite && rel > tol) {
     ite = ite + 1;
-    Jacobi(N, h, u, f);
-    double res = Residual(N, u, f, s);
-    rel = res/res0;
-    printf("%10d %10f \n", ite, res);
+    Jacobi(N, h, u, f, s);
+    // res = Residual(N, u, f);
+    // rel = res/res0;
+    // printf("%10d %10f \n", ite, res);
   }
+  res = Residual(N, u, f);
+  printf("res0 = %f, res =  %f, factor = %f\n", res0, res, res0/res);
 }
 
 int main(int argc, char** argv) {
-  long N = read_option<long>("-N", argc, argv);
-  long max_ite = read_option<long>("-ite", argc, argv, "5000");
+  long N, max_ite;
+  sscanf(argv[1], "%d", &N);
+  sscanf(argv[2], "%d", &max_ite);
+
 
   double* f = (double*) malloc((N+2)*(N+2) * sizeof(double));
   double* u = (double*) malloc((N+2)*(N+2) * sizeof(double));
@@ -89,15 +92,17 @@ int main(int argc, char** argv) {
 
 
   const double tol = 1e-6;
-  double res0 = Residual(N, u, f);
+
 
   #ifdef _OPENMP
-    tt = omp_get_wtime();
+    double tt = omp_get_wtime();
   #else
     Timer t;
     t.tic();
   #endif
+
   Jacobi_iter(N, u, f, s, max_ite, tol);
+
   #ifdef _OPENMP
     double time = omp_get_wtime() - tt;
   #else
@@ -105,8 +110,6 @@ int main(int argc, char** argv) {
   #endif
   printf("time = %f\n", time);
 
-  double res = Residual(N, u, f);
-  printf("factor = %f\n", res0/res);
 
   free(f);
   free(u);
